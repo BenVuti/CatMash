5c7c2f6b5ba1726429e350ef7aa7fbadce6ae779
Theo Benvenuti <theo.benvenuti@outlook.com>
2017-07-10 21:13:06 +0200

    + add classes Cat and Mash + display two hardcoded cats

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
 
+export class Mash {
+  leftCat: Cat;
+  rightCat: Cat;
+}
+
+export class Cat {
+  id: string;
+  imageSource: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +17,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My First Angular App';
+  mash: Mash = {
+    leftCat: {
+      id: "sdsd",
+      imageSource: "http://24.media.tumblr.com/tumblr_m82woaL5AD1rro1o5o1_1280.jpg"
+    },
+    rightCat: {
+      id: "dsds",
+      imageSource: "http://24.media.tumblr.com/tumblr_m29a9d62C81r2rj8po1_500.jpg"
+    }
+  };
 }
